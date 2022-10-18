a3f161f8cb9bf67bd4e98a8eab6309d1b1676492
Xpomin <s-hromin@mail.ru>
2022-10-18 19:29:05 +0700

    menu 70 percent is ready

--- Project/src/Program.cs
+++ Project/src/Program.cs
@@ -17,9 +17,7 @@ public class Program
         while (input != 'q')
         {
             PrintMenuOptions();
-            Console.Write("Введите команду:");
-            input = Console.ReadKey().KeyChar;
-            Console.WriteLine();
+            input = UtilsForGenerators.Read("Введите команду:", 2)[0];
             switch (input)
             {
 
@@ -36,6 +34,8 @@ public class Program
                     break;
                 case '4':
                     break;
+                case '5':
+                    break;
                 case 'q':
                     break;
                 default:
@@ -46,8 +46,6 @@ public class Program
         }
     }
 
-
-    // переделать
     public static void PrintGensInProgram()
     {
         Console.WriteLine("Генераторы с постоянным шагом:");
@@ -56,10 +54,8 @@ public class Program
         PrintGens(ProgramRandomGens.AsEnumerable());
         Console.WriteLine("Композитные генераторы:");
         bool CompositionGenIsFull = PrintGens(ProgramCompositionGens.AsEnumerable());
-        if (CompositionGenIsFull)
-        {
-            PrintGensInCompositeGen(ProgramCompositionGens);
-        }
+        //   if (CompositionGenIsFull)
+        //       PrintGensInCompositeGen(ProgramCompositionGens);
     }
 
     public static bool PrintGens(IEnumerable<BaseGen> gen)
@@ -80,11 +76,9 @@ public class Program
 
     public static void PrintGensInCompositeGen(List<CompositionGen> gen)
     {
-        foreach (var item in gen)
-        {
-            Console.WriteLine($"В композитном генераторе с именем {item.Name} находятся генераторы:");
-
-        }
+        Console.WriteLine("ERROR");
+        //foreach (var item in gen)
+        //Console.WriteLine($"В композитном генераторе с именем {item.Name} находятся генераторы:");
     }
 
 
@@ -93,12 +87,11 @@ public class Program
     public static void MenuPointAddDelGen()
     {
         char input = 'a';
+
         while (input != 'q')
         {
             PrintOptionsAddDelGen();
-            Console.Write("Введите команду:");
-            input = Console.ReadKey().KeyChar;
-            Console.WriteLine();
+            input = UtilsForGenerators.Read("Введите команду:", 2)[0];
             switch (input)
             {
                 case '0':
@@ -111,18 +104,10 @@ public class Program
                     ProgramCompositionGens.Add(UtilsForGenerators.CreateCompositionGen());
                     break;
                 case '3':
-                    Console.WriteLine("Раздел в работе");
-                    string CompGenName = UtilsForGenerators.Read("Введите имя композитного генератора:", 2);
-                    string GenToDelete = UtilsForGenerators.Read("Введите имя генератора, который хотите удалить из композитного:", 2);
-                    try
-                    {
-                        ProgramCompositionGens[ProgramCompositionGens.FindIndex(gen => gen.Name == CompGenName)].DeleteGenByName(GenToDelete);
-                    }
-                    catch (IndexOutOfRangeException)
-                    {
-                        Console.WriteLine("Генератор с таким именем не был найден");
-                    }
-
+                    AddGenToProgramCompositGen();
+                    break;
+                case '4':
+                    DelGenInProgramCompositGen();
                     break;
                 case 'q':
                     break;
@@ -135,15 +120,55 @@ public class Program
     }
 
 
+    public static void AddGenToProgramCompositGen()
+    {
+        if (ProgramCompositionGens.Any())
+        {
+            string GenNameToAdd = UtilsForGenerators.Read("Введите имя композитного генератора, в который будет добавляться генератор:", 2);
+            char genDecision = UtilsForGenerators.Read("Вы хотите добавить генератор c постоянным шагом? y/n:", 2)[0];
+            if (genDecision == 'y') GetProgramCompositGenByName(GenNameToAdd).PushGen(UtilsForGenerators.CreateConstStepGen());
+            else GetProgramCompositGenByName(GenNameToAdd).PushGen(UtilsForGenerators.CreateRandomGen());
+        }
+        else
+        {
+            Console.WriteLine("Нет созданных компизитных генераторов");
+
+        }
+    }
+
+    public static void DelGenInProgramCompositGen()
+    {
+        if (ProgramCompositionGens.Any())
+        {
+            string CompositGenNameToWork = UtilsForGenerators.Read("Введите имя композитного генератора, в котором будет удаляться:", 2);
+            string GenNameToDoOperation = UtilsForGenerators.Read("Введите имя генератора, который хотите удалить из композитного:", 2);
+            try
+            {
+                GetProgramCompositGenByName(CompositGenNameToWork).DeleteGenByName(GenNameToDoOperation);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Генератор с таким именем не был найден");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Нет созданных компизитных генераторов");
+        }
+    }
 
 
-
+    private static CompositionGen GetProgramCompositGenByName(string name)
+    {
+        return ProgramCompositionGens[ProgramCompositionGens.FindIndex(gen => gen.Name == name)];
+    }
 
 
     public static void PrintOptionsAddDelGen() => Console.WriteLine("\t0 - Добавить генератор с постоянным шагом\n" +
                                                                      "\t1 - Добавить генератор псевдослучаных чисел\n" +
                                                                      "\t2 - Добавить композитный генератор\n" +
-                                                                     "\t3 - Удалить генератор из композитного генератора\n" +
+                                                                     "\t3 - Добавить генератор в композитный генератор" +
+                                                                     "\t4 - Удалить генератор из композитного генератора\n" +
                                                                      "\tq - Выйти из данного пункта меню");
 
 
@@ -158,10 +183,10 @@ public class Program
     public static void PrintMenuOptions() => Console.WriteLine("\n\nМеню программы:\n" +
                                                            "\t0 - Как работают проргамма и генераторы\n" +
                                                            "\t1 - Вывод списка генераторов\n" +
-                                                           "\t2 - Добавление генератора для использования\n" +
+                                                           "\t2 - Добавление/удаление генератора\n" +
                                                            "\t3 - Подсчет среднего числа у генератора\n" +
-                                                           "\t7 - Добавление генератора в композитный генератор\n" +
-                                                           "\t8 - Удаление генератора из композитного генератора(по имени/индексу)\n" +
+                                                           "\t4 - Добавление генератора в композитный генератор\n" +
+                                                           "\t5 - Удаление генератора из композитного генератора(по имени/индексу)\n" +
                                                            "\tq - Выход\n");
 
 
@@ -172,8 +197,7 @@ public class Program
         while (input != 'q')
         {
             PrintOptionsHowProgramWorks();
-            Console.Write("Введите команду:");
-            input = Console.ReadKey().KeyChar;
+            input = UtilsForGenerators.Read("Введите команду:", 2)[0];
             Console.WriteLine();
             switch (input)
             {
